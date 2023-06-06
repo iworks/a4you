90100bb1913daa6f5c615d2a04bfa01ab0515a3b
Marcin Pietrzak <marcin@iworks.pl>
2023-06-06 14:43:53 +0200

    options

--- etc/options.php
+++ etc/options.php
@@ -5,16 +5,30 @@ function iworks_a4wp_options() {
 	/**
 	 * main settings
 	 */
-    $parent = 'settings.php';
-
 	$options['index'] = array(
-		'version'  => '0.0',
-		'page_title' => __( 'Configuration', 'a4wp' ),
-		'menu' => 'submenu',
-		'parent' => $parent,
-		'options'  => array(),
-		'metaboxes' => array(),
-		'pages' => array(),
+		'version'    => '0.0',
+		'use_tabs'   => true,
+		'page_title' => __( 'Analitics 4 WP', 'a4wp' ),
+		'menu'       => 'options',
+		'options'    => array(
+			array(
+				'type'  => 'heading',
+				'label' => __( 'General', 'a4wp' ),
+			),
+			array(
+				'name'              => 'tag_id',
+				'type'              => 'text',
+				'th'                => __( 'Google Tag ID', 'a4wp' ),
+				'placeholder'       => 'G-xxxxxxxxxx',
+				'sanitize_callback' => 'esc_html',
+			),
+			array(
+				'type'  => 'heading',
+				'label' => __( 'Content', 'a4wp' ),
+			),
+		),
+		'metaboxes'  => array(),
+		'pages'      => array(),
 	);
 	return $options;
 }
